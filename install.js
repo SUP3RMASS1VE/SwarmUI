b24751e3e229658cf29a061d52f5f932fa5bbe7b
SUP3RMASS1VE <Martincull@live.com>
2024-12-15 16:47:37 +0000

    Update install.js

--- install.js
+++ install.js
@@ -9,16 +9,14 @@ module.exports = {
         ]
       }
     },
-    
-    // Edit this step with your custom install commands
     {
-      method: "shell.run",
+      method: "script.start",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          
-        ]
+        uri: "torch.js",
+        params: {
+          venv: "env",  // Virtual environment folder path
+          path: "app",  // Path to start the shell from
+        }
       }
     },
     {
